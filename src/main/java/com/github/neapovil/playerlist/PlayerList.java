aa05cfbac79e31405830f964865b5a7e0c88a3aa
Nearata <nearata@protonmail.com>
2022-02-27 00:28:56 +0100

    update 1.18.1

--- src/main/java/com/github/neapovil/playerlist/PlayerList.java
+++ src/main/java/com/github/neapovil/playerlist/PlayerList.java
@@ -9,6 +9,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import com.electronwill.nightconfig.core.file.FileConfig;
 
+import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.arguments.BooleanArgument;
+import dev.jorel.commandapi.arguments.GreedyStringArgument;
+import dev.jorel.commandapi.arguments.LiteralArgument;
+import dev.jorel.commandapi.arguments.MultiLiteralArgument;
+import net.kyori.adventure.text.Component;
 import net.md_5.bungee.api.ChatColor;
 
 public final class PlayerList extends JavaPlugin implements Listener
@@ -31,6 +37,33 @@ public final class PlayerList extends JavaPlugin implements Listener
         this.config.load();
 
         this.getServer().getPluginManager().registerEvents(this, this);
+
+        new CommandAPICommand("tablist")
+                .withPermission("playerlist.command")
+                .withArguments(new MultiLiteralArgument("header", "footer"))
+                .withArguments(new GreedyStringArgument("text"))
+                .executes((sender, args) -> {
+                    final String setting = (String) args[0];
+                    final String text = (String) args[1];
+
+                    this.config.set("general." + setting, text);
+
+                    sender.sendMessage(setting + " changed to: " + text);
+                })
+                .register();
+
+        new CommandAPICommand("tablist")
+                .withPermission("playerlist.command")
+                .withArguments(new LiteralArgument("enabled"))
+                .withArguments(new BooleanArgument("bool"))
+                .executes((sender, args) -> {
+                    final boolean bool = (boolean) args[0];
+
+                    this.config.set("general.enabled", bool);
+
+                    sender.sendMessage("Custom playerlist enabled: " + bool);
+                })
+                .register();
     }
 
     @Override
@@ -46,8 +79,14 @@ public final class PlayerList extends JavaPlugin implements Listener
     @EventHandler
     public void playerJoin(PlayerJoinEvent event)
     {
+        if (!((boolean) this.config.get("general.enabled")))
+        {
+            return;
+        }
+
         final String header = ChatColor.translateAlternateColorCodes('&', this.config.get("general.header"));
         final String footer = ChatColor.translateAlternateColorCodes('&', this.config.get("general.footer"));
-        event.getPlayer().setPlayerListHeaderFooter(header, footer);
+
+        event.getPlayer().sendPlayerListHeaderAndFooter(Component.text(header), Component.text(footer));
     }
 }
